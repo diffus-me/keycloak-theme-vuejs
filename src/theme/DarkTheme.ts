54d6395400060fa8e65126e677de0927494ffb4c
Edward Cui <edward.cui@graviti.cn>
2024-10-11 16:52:01 +0800

    Add another dark theme

--- src/theme/DarkTheme.ts
+++ src/theme/DarkTheme.ts
@@ -1,4 +1,4 @@
-import type { ThemeTypes } from '~/types/themeTypes/ThemeType';
+import type { ThemeTypes } from '~/types/themeTypes/ThemeType'
 
 const DarkOrangeTheme: ThemeTypes = {
   name: 'DarkOrangeTheme',
@@ -33,14 +33,14 @@ const DarkOrangeTheme: ThemeTypes = {
     primary200: '#8bacad',
     secondary200: '#e3bf91'
   }
-};
+}
 
 const DarkPurpleTheme: ThemeTypes = {
   name: 'DarkPurpleTheme',
   dark: true,
   variables: {
     'high-opacity': 1,
-    'light-opacity': 0.12,
+    'light-opacity': 0.12
   },
   colors: {
     primary: '#cccccc',
@@ -64,7 +64,7 @@ const DarkPurpleTheme: ThemeTypes = {
     darkprimary: '#555555',
     darksecondary: '#293189',
     darkText: '#d7dcec',
-    lightText: '#bdc8f0',
+    lightText: '#ffffff80',
     borderLight: '#404968',
     inputBorder: '#5f698d',
     containerBg: '#1a223f',
@@ -76,6 +76,49 @@ const DarkPurpleTheme: ThemeTypes = {
     primary200: '#90caf9',
     secondary200: '#b39ddb'
   }
-};
+}
 
-export { DarkOrangeTheme, DarkPurpleTheme };
+const DarkWhiteTheme: ThemeTypes = {
+  name: 'DarkWhiteTheme',
+  dark: true,
+  variables: {
+    'high-opacity': 1,
+    'light-opacity': 0.12
+  },
+  colors: {
+    primary: '#4353ff',
+    secondary: '#cccccc',
+    surface: '#121212',
+    background: '#000000',
+    'subtitle-text-color': '#ffffff',
+    'carousel-dot-color': '#ffffff',
+    'carousel-title-color': '#ffffff',
+    'carousel-subtitle-color': '#e6e6e6cc',
+    info: '#03c9d7',
+    success: '#05b187',
+    accent: '#fc4b6c',
+    warning: '#fec90f',
+    error: '#fc4b6c',
+    lightprimary: '#6e7aff',
+    lightsecondary: '#ffffff',
+    lightsuccess: '#143c33',
+    lighterror: '#f9d8d8',
+    lightwarning: '#fff8e1',
+    darkprimary: '#293189',
+    darksecondary: '#555555',
+    darkText: '#444444',
+    lightText: '#ffffff80',
+    borderLight: '#404968',
+    inputBorder: '#5f698d',
+    containerBg: '#1a223f',
+    'on-surface-variant': '#111936',
+    facebook: '#4267b2',
+    twitter: '#1da1f2',
+    linkedin: '#0e76a8',
+    gray100: '#19203c',
+    primary200: '#90caf9',
+    secondary200: '#b39ddb'
+  }
+}
+
+export { DarkOrangeTheme, DarkPurpleTheme, DarkWhiteTheme }
